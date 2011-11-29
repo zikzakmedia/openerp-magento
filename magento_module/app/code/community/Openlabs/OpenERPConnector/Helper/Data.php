e6fade905b7f0e125fb5a35a4de2457073da7fb1
resteve <resteve@zikzakmedia.com>
2011-11-29 08:24:47 +0100

    [DEL] Remove Magento Module Webservices

--- magento_module/app/code/community/Openlabs/OpenERPConnector/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Openlabs_OpenERPConnector_Helper_Data extends Mage_Core_Helper_Abstract
-{
-	//Linux4ever_MagentoXtender_Model_Api
-}
-?>
--- magento_module/app/code/community/Openlabs/OpenERPConnector/controllers/OpenerpconnectorController.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Openlabs_OpenERPConnector_OpenerpconnectorController extends Mage_Core_Controller_Front_Action
-{
-
-}
